0ca05862988c871ca05d2854047f34f8aa5f054a
Emy2002-girl <ranboukiman805@gmail.com>
2025-10-11 17:59:29 +0100

    new travaille

--- Atelier1.cpp
+++ Atelier1.cpp
@@ -1,5 +1,55 @@
 #include<iostream>
 using namespace std;
+void remplir(int *arr, int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout << "entrer l'element " << i + 1 << " du tableau :";
+        cin >> *(arr + i);
+    }
+    }
+void afficher(const int *arr,int n){
+    for (int i = 0; i < n; i++){
+        cout << "les élements du tableau sont : " <<arr[i] <<"\n";
+    }
+}
+int& trouverMax(int *arr, int n) {
+    int indexMax = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > arr[indexMax]) {
+            indexMax = i;
+        }
+    }
+
+    return arr[indexMax];
+}
+// void inverser(int* arr, int n) {
+//     int* debut = arr;          
+//     int* fin = arr + n - 1;   
+
+//     while (debut < fin) {
+//         int temp = *debut;
+//         *debut = *fin;
+//         *fin = temp;
+
+//         debut++;
+//         fin--;
+//     }
+// }
+void inverserAvecTemp(int* arr, int n) {
+    int* temp = new int[n]; // tableau temporaire
+
+    for (int i = 0; i < n; i++) {
+        temp[i] = arr[n - 1 - i];
+    }
+
+    
+    for (int i = 0; i < n; i++) {
+        arr[i] = temp[i];
+    }
+
+    delete[] temp;
+}
 int main()
 {
     // Exercice 1
@@ -51,6 +101,25 @@ int main()
 // j = 5 ;
 // n = i *= --j ;
 // cout << "E : i = " << i << " j = " << j << " n = " << n << "\n" ; E :i = 12  j = 4 n = 12
-    return 0;
+   //Exercice 5 :
+    int i,n;
+    cout << "entrer la taille du tableau";
+    cin >> n;
+    int *arr = new int(n);
+    remplir(arr, n);
+    afficher(arr, n);
+    int &maxRef = trouverMax(arr, n);
+    cout << "Le maximum est : " << &maxRef << endl;
+    // inverser(arr, n);
+    // cout << "les elements inverser du tableau sont :";
+    // for (int i = 0; i < n; i++){
+    //      cout <<arr[i];
+    // }
+    inverserAvecTemp(arr, n);
 
+    cout << "Tableau inversé avec tableau temporaire : ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+        return 0;
 }
