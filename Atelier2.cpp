a01ccea506d1bf095e23d734b795fc51754a1fe2
imane ranbok <imane.ranbok@etu.uae.ac.ma>
2025-10-26 21:06:22 +0100

    add changes

--- Atelier2.cpp
+++ Atelier2.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <new>
+#include <cmath>
 using namespace std;
 //   ------------------------------------------------------------------------------------EXERCICE1 ---------------------------------------------------------
 // int i = 0;
@@ -102,12 +103,111 @@ using namespace std;
 //             return 0;
 // }
 //  -------------------------------------------------------------------- EXERCICE5-----------------------------------------------------------------------
-int main(){
-    int a;
-    cout << "donner une valeur de a :";
-    cin >> a;
-    int &ref_a = a;
-    int *ref_a = &a;
-    cout << ref_a; //reference of a 
-    // cout << *ref_a; 
-}
+// int main(){
+//     int a;
+//     cout << "donner une valeur de a :";
+//     cin >> a;
+//     int &ref_a = a;
+//     int *p = &a;
+// cout << "la valeur de a : " << a;
+// cout << " la reference vers a :" << ref_a;
+// cout << " la valeur pointee" << *p;
+// return 0;
+// }
+//  -------------------------------------------------------------------- EXERCICE8-----------------------------------------------------------------------
+
+// class Voiture{
+//      public:
+//          string marque;
+//          string modele;
+//          int annee;
+//          float kilometrage;
+//          float vitesse;
+//          public:
+//          Voiture(){
+//              marque = "unkown";
+//              modele =  "unkown";
+//              annee = 0;
+//              kilometrage = 0.0;
+//              vitesse = 0.0;
+//          }
+//          Voiture(string marque, string modele, int annee,float kilometrage,float vitesse){
+//              this->marque = marque;
+//              this->modele = modele;
+//              this->annee = annee;
+//              this->kilometrage = kilometrage;
+//              this->vitesse = vitesse;
+//          }
+//          float accelerer( float valeur){
+//              return vitesse +=valeur;
+//          }
+//          void freiner(float valeur){
+//                  vitesse -= valeur;
+//                  if(vitesse <0){
+//                      vitesse = 0;
+//                  }
+//          }
+//          float avancer(float distance){
+//              return kilometrage += distance;
+//          }
+//          void afficherInfo(){
+//              cout <<marque<<" "<<modele <<" "<<annee <<" "<< kilometrage <<" "<< vitesse << "\n";
+//          }
+//         ~Voiture(){
+//             cout << "la voiture est detruite.";
+//         }
+// };
+
+//     int main(){
+//     Voiture voiture1;
+//     Voiture voiture2("Toyota"," Corolla",2018,35000,50);
+//     voiture2.accelerer(20);
+//     voiture2.freiner(10);
+//     voiture2.accelerer(45.5);
+//     cout << "voiture1:\n";
+//     voiture1.afficherInfo();
+//     cout << "voiture2:\n";
+//     voiture2.afficherInfo();
+    
+//  -------------------------------------------------------------------- EXERCICE9-----------------------------------------------------------------------
+
+//  class vecteur3d{
+//     public :
+//         float x, y, z;
+//     public :
+//         vecteur3d( float a = 0.0, float b = 0.0 , float c = 0.0)
+//         {
+//             x = a;
+//             y = b;
+//             z = c;
+//         }
+//         void afficher(){
+//             cout << "(" << x <<","<< y <<","<< z <<")"<< "\n";
+//         }
+//         vecteur3d somme(vecteur3d v){
+//             vecteur3d resultat1; // resultat is the type of class 
+//             resultat1.x = x + v.x;
+//             resultat1.y = y + v.y;
+//             resultat1.z = z + v.z;
+//             return resultat1;
+//         }
+//         float produitScalaire(vecteur3d v){
+//             return x*v.x + y*v.y + z*v.z;
+//         }
+//         bool coincide(vecteur3d v){
+//             return (x == v.x && y == v.y && z == v.z);
+//         }
+//         float norme() {
+//         return sqrt(x*x + y*y + z*z);
+//     }
+    
+// };
+//  int main(){
+//      vecteur3d vecteur1(1,2,3);
+//      vecteur3d vecteur2(4,5,6);
+     
+//     vecteur3d resultat1 =vecteur1.somme(vecteur2);
+//     resultat1.afficher();
+//     cout << vecteur1.produitScalaire(vecteur2) << "\n";
+//     cout << (vecteur1.coincide(vecteur2) ? "Oui" : "Non") << "\n";
+//  }
