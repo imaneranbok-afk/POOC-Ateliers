d8a465078e157f7f48a3995989881e114cbb59a6
imane ranbok <imane.ranbok@etu.uae.ac.ma>
2025-11-10 21:44:37 +0100

    add changes

--- Atelier2.cpp
+++ Atelier2.cpp
@@ -114,6 +114,27 @@ using namespace std;
 // cout << " la valeur pointee" << *p;
 // return 0;
 // }
+// ----------------------------------------------------Exercice 7-------------------------------------------------------------
+
+// int n;
+// int factorielle(int n){
+//          if (n <= 1)
+//         return 1;
+//     else
+//         return n * factorielle(n - 1);
+//     }
+// string chaine;
+// int permutation(string chaine){
+//     int t = chaine.length();
+//     cout << "le nombre des permutation est : \n";
+//     return factorielle(t);
+// }
+
+// int main(){
+   
+//     cout << permutation("IMANE");
+//     return 0;
+// }
 //  -------------------------------------------------------------------- EXERCICE8-----------------------------------------------------------------------
 
 // class Voiture{
